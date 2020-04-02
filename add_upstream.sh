92236f141cc82d2f78f7d16bc5113e4c96f2e8d7
Walter Pearce <jaynus@gmail.com>
2020-04-02 16:42:41 -0700

    Add remotes and upstreams

--- add_upstream.sh
+++ add_upstream.sh
@@ -5,8 +5,26 @@ popd
 
 pushd external/ultraviolet
 git remote add upstream https://github.com/termhn/ultraviolet.git
+git remote add github git@github.com:jaynus/ultraviolet.git
 popd
 
 pushd external/type-uuid
 git remote add upstream https://github.com/randomPoison/type-uuid.git
+git remote add github git@github.com:jaynus/type-uuid.git
+popd
+
+pushd external/purple
+git remote add github git@github.com:jaynus/purple.git
+popd
+
+pushd external/graphy
+git remote add github git@github.com:jaynus/graphy.git
+popd
+
+pushd external/game-metrics
+git remote add github git@github.com:jaynus/game-metrics.git
+popd
+
+pushd external/std140
+git remote add upstream https://github.com/RSSchermer/std140.rs
 popd
